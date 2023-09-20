2dc60cfefe0433e2b970415bf6b8caf86b48be99
Bui Quoc Cong <congbui123123@gmail.com>
2023-09-20 15:51:15 +0700

    Buoi 5

---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace _07_2001210455_BuiQuocCong
+{
+    partial class Bai1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(14, 159);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(24, 23);
+            this.btnCong.TabIndex = 6;
+            this.btnCong.Text = "&+";
+            this.btnCong.UseVisualStyleBackColor = true;
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(55, 159);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(24, 23);
+            this.btnTru.TabIndex = 7;
+            this.btnTru.Text = "&-";
+            this.btnTru.UseVisualStyleBackColor = true;
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(97, 159);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(24, 23);
+            this.btnNhan.TabIndex = 8;
+            this.btnNhan.Text = "&*";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            // 
+            // btnChia
+            // 
+            this.btnChia.Location = new System.Drawing.Point(143, 159);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(24, 23);
+            this.btnChia.TabIndex = 9;
+            this.btnChia.Text = "&/";
+            this.btnChia.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "b =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kết Quả: ";
+            // 
+            // txtA
+            // 
+            this.txtA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtA.Location = new System.Drawing.Point(41, 35);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 1;
+            // 
+            // txtB
+            // 
+            this.txtB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtB.Location = new System.Drawing.Point(50, 87);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 3;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKQ.Location = new System.Drawing.Point(67, 122);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 20);
+            this.txtKQ.TabIndex = 5;
+            // 
+            // Bai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Name = "Bai1";
+            this.Text = "Cộng Trừ Nhân Chia";
+            this.Load += new System.EventHandler(this.Bai1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtKQ;
+
+    }
+}
---
+++ 07_2001210455_BuiQuocCong/07_2001210455_BuiQuocCong/Bai1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_2001210455_BuiQuocCong
+{
+    public partial class Bai1 : Form
+    {
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private double Cong (double a, double b)
+        {
+            return a + b;
+        }
+        private double Tru (double a, double b)
+        {
+            return a - b;
+        }
+        private double Nhan (double a, double b)
+        {
+            return a * b;
+        }
+        private double Chia (double a, double b)
+        {
+            return a * 1.0 / b;
+        }
+        private void btnCong_Click(o)
+
+    }
+}
